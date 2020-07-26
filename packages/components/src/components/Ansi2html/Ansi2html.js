e11f3b154c05007bbbbde052c99eabaef777a0b5
Steve Soares <soaresss@ibm.com>
2020-07-26 11:52:15 -0400

    Add ansi parser

--- packages/components/src/components/Ansi2html/Ansi2html.js
+++ packages/components/src/components/Ansi2html/Ansi2html.js
@@ -12,44 +12,263 @@ limitations under the License.
 */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { escapeCarriageReturn } from 'escape-carriage';
-import { parse } from 'ansicolor';
-import Linkify from 'react-linkify';
+import { colors, textStyles } from './defaults';
 
-import './Ansi.scss';
-
-const convertCSSToStyle = css => {
-  if (css === '') {
-    return {};
-  }
-  const style = {};
-  css.split(';').forEach(element => {
-    if (element !== '') {
-      const [key, value] = element.split(':');
-      style[key] = value;
-    }
-  });
-  return style;
-};
-
-const logMessage = line => {
-  const escapedText = escapeCarriageReturn(line);
-  const parsed = parse(escapedText);
-  return (
-    <Linkify>
-      {parsed.spans.map(({ text, css }) => {
-        const attributes = {
-          text,
-          style: convertCSSToStyle(css)
-        };
-        return <span style={attributes.style}>{attributes.text}</span>;
-      })}
-    </Linkify>
-  );
-};
+const linkifyIt = require('linkify-it')().tlds(require('tlds'));
 
 const Ansi2html = ({ children }) => {
-  return logMessage(children);
+  let properties = {
+    foregroundColor: null,
+    backgroundColor: null,
+    bold: false,
+    italic: false,
+    underline: false,
+    conceal: false,
+    cross: false
+  };
+
+  let styles = {};
+  let text = '';
+  let line = [];
+
+  const reset = () => {
+    properties = {
+      foregroundColor: null,
+      backgroundColor: null,
+      bold: false,
+      italic: false,
+      underline: false,
+      conceal: false,
+      cross: false
+    };
+  };
+
+  const enable = flag => {
+    properties[flag] = true;
+  };
+
+  const disable = flag => {
+    properties[flag] = false;
+  };
+
+  const getXtermColorClass = commandStack => {
+    if (commandStack.length >= 2 && commandStack[0] === '5') {
+      commandStack.shift();
+      const colorIndex = +commandStack.shift();
+      if (colorIndex >= 0 && colorIndex <= 255) {
+        return colors[colorIndex];
+      }
+    }
+    return null;
+  };
+
+  const getTermColorClass = (colorIndex, type) => {
+    if (colorIndex > 7) {
+      return null;
+    }
+    if (type === 'l') {
+      return colors[colorIndex + 8];
+    }
+    return colors[colorIndex];
+  };
+
+  const setFGColor = (type, command) => {
+    properties.foregroundColor = getTermColorClass(type, command);
+  };
+
+  const setBGColor = (type, command) => {
+    properties.backgroundColor = getTermColorClass(type, command);
+  };
+
+  const setFGColor256 = commandStack => {
+    properties.foregroundColor = getXtermColorClass(commandStack);
+  };
+
+  const setBGColor256 = commandStack => {
+    properties.backgroundColor = getXtermColorClass(commandStack);
+  };
+
+  const setStyle = (command, stack) => {
+    const setProperties = {
+      0: () => reset(),
+      1: () => enable('bold'),
+      3: () => enable('italic'),
+      4: () => enable('underline'),
+      8: () => enable('conceal'),
+      9: () => enable('cross'),
+
+      21: () => disable('bold'),
+      22: () => disable('bold'),
+      23: () => disable('italic'),
+      24: () => disable('underline'),
+      28: () => disable('conceal'),
+      29: () => disable('cross'),
+
+      30: () => setFGColor(0),
+      31: () => setFGColor(1),
+      32: () => setFGColor(2),
+      33: () => setFGColor(3),
+      34: () => setFGColor(4),
+      35: () => setFGColor(5),
+      36: () => setFGColor(6),
+      37: () => setFGColor(7),
+      38: s => setFGColor256(s),
+      39: () => setFGColor(9),
+
+      40: () => setBGColor(0),
+      41: () => setBGColor(1),
+      42: () => setBGColor(2),
+      43: () => setBGColor(3),
+      44: () => setBGColor(4),
+      45: () => setBGColor(5),
+      46: () => setBGColor(6),
+      47: () => setBGColor(7),
+      48: s => setBGColor256(s),
+      49: () => setBGColor(9),
+
+      90: () => setFGColor(0, 'l'),
+      91: () => setFGColor(1, 'l'),
+      92: () => setFGColor(2, 'l'),
+      93: () => setFGColor(3, 'l'),
+      94: () => setFGColor(4, 'l'),
+      95: () => setFGColor(5, 'l'),
+      96: () => setFGColor(6, 'l'),
+      97: () => setFGColor(7, 'l'),
+      99: () => setFGColor(9, 'l'),
+
+      100: () => setBGColor(0, 'l'),
+      101: () => setBGColor(1, 'l'),
+      102: () => setBGColor(2, 'l'),
+      103: () => setBGColor(3, 'l'),
+      104: () => setBGColor(4, 'l'),
+      105: () => setBGColor(5, 'l'),
+      106: () => setBGColor(6, 'l'),
+      107: () => setBGColor(7, 'l'),
+      109: () => setBGColor(9, 'l')
+    };
+
+    if (setProperties[command]) {
+      setProperties[command](stack);
+    }
+  };
+
+  const evaluateCommandStack = stack => {
+    const command = stack.shift();
+    if (!command) {
+      return;
+    }
+    setStyle(command, stack);
+    evaluateCommandStack(stack);
+  };
+
+  const linkify = (str, className) => {
+    if (!str) {
+      return null;
+    }
+    const matches = linkifyIt.match(str);
+    if (!matches) {
+      return <span style={className}>{str}</span>;
+    }
+    const elements = [];
+    let offset = 0;
+    matches.forEach(match => {
+      if (match.index > offset) {
+        const string = str.substring(offset, match.index);
+        elements.push(<span style={className}>{string}</span>);
+      }
+      elements.push(
+        <a href={match.url} style={className}>
+          {match.text}
+        </a>
+      );
+      offset = match.lastIndex;
+    });
+
+    if (str.length > offset) {
+      const string = str.substring(offset, str.length);
+      elements.push(<span style={className}>{string}</span>);
+    }
+    return elements;
+  };
+
+  const handleSequence = s => {
+    const indicator = s[1];
+    const commands = s[2].split(';');
+    const terminator = s[3];
+
+    if (indicator !== '[' && terminator !== 'm') {
+      return;
+    }
+
+    const tag = linkify(text, styles);
+    if (tag) {
+      line = line.concat(tag);
+    }
+
+    text = '';
+    styles = {};
+
+    if (commands.length === 0) {
+      reset();
+    }
+
+    evaluateCommandStack(commands);
+
+    styles = {
+      color: properties.foregroundColor,
+      backgroundColor: properties.backgroundColor
+    };
+
+    Object.keys(textStyles).forEach(key => {
+      if (properties[key]) {
+        styles = {
+          ...styles,
+          ...textStyles[key]
+        };
+      }
+    });
+  };
+
+  const parse = (ansi, index) => {
+    // eslint-disable-next-line no-control-regex
+    const ansiRegex = new RegExp('^\u001b([@-_])(.*?)([@-~])');
+    const characterRegex = new RegExp('.', 'm');
+    let offset = 0;
+    while (offset !== ansi.length) {
+      const str = ansi.substring(offset);
+      const controlSequence = str.match(ansiRegex);
+      if (controlSequence) {
+        offset += controlSequence.index + controlSequence[0].length;
+        handleSequence(controlSequence);
+      } else {
+        const character = str.match(characterRegex);
+        text += (character && character[0]) || '';
+        offset += 1;
+      }
+    }
+    const end = linkify(text, styles);
+
+    return (
+      <div key={index}>
+        {line} {end}
+      </div>
+    );
+  };
+
+  const convert = ansi => {
+    const body = [];
+    ansi.split(/\r?\n/).forEach((part, index) => {
+      const tags = parse(part, index);
+      body.push(tags);
+
+      // reset text
+      text = '';
+      line = [];
+    });
+    return body;
+  };
+
+  return convert(children);
 };
 
 Ansi2html.propTypes = {
--- packages/components/src/components/Ansi2html/Ansi2html.test.js
+++ packages/components/src/components/Ansi2html/Ansi2html.test.js
@@ -12,51 +12,252 @@ limitations under the License.
 */
 
 import React from 'react';
-import colors from 'colors';
 import { renderWithIntl } from '../../utils/test';
 import Ansi2html from './Ansi2html';
 
-colors.enable();
+const getElement = (text, query) => {
+  const { queryByText } = renderWithIntl(<Ansi2html>{text}</Ansi2html>);
+  const queryRegex = new RegExp(query, 'i');
+  return queryByText(queryRegex);
+};
 
 describe('Ansi2html', () => {
-  it('can display text', () => {
-    const { queryByText } = renderWithIntl(<Ansi2html>hello world</Ansi2html>);
-    const element = queryByText(/hello world/i);
-    expect(element.tagName).toBe('SPAN');
+  it('displays text', () => {
+    const element = getElement('Hello World', 'Hello World');
     expect(element).toBeTruthy();
   });
 
-  it('can convert ANSI escape codes to multiple class names', () => {
-    const { queryByText } = renderWithIntl(
-      <Ansi2html>{'hello world'.red.underline.bgBlack}</Ansi2html>
-    );
-    const element = queryByText(/hello world/i);
-    expect(element).toBeTruthy();
+  it('displays green text', () => {
+    const element = getElement('\u001b[32mHello World\u001b[0m', 'Hello World');
     expect(element.outerHTML).toBe(
-      '<span style="text-decoration: underline; color: rgb(204, 0, 0); background: rgb(0, 0, 0);">hello world</span>'
+      '<span style="color: rgb(0, 128, 0);">Hello World</span>'
     );
   });
 
-  it('has multiple elements for line seperated logs', () => {
-    const { queryByText } = renderWithIntl(
-      <Ansi2html>
-        {`${'hello world1'.red.underline.bgBlack}\n${
-          'hello world2'.blue.italic.bgCyan
-        }`}
-      </Ansi2html>
+  it('displays red text', () => {
+    const element = getElement('\u001b[31mHello World\u001b[0m', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 0, 0);">Hello World</span>'
     );
-    queryByText(/hello world1/i).toBeTruthy();
-    queryByText(/hello world2/i).toBeTruthy();
   });
 
-  it('can have links', () => {
-    const { queryByText } = renderWithIntl(
-      <Ansi2html>this is a link https://google.com</Ansi2html>
+  it('displays yellow text', () => {
+    const element = getElement('\u001b[33mHello World\u001b[0m', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 128, 0);">Hello World</span>'
     );
-    const element = queryByText(/this is a link/i);
-    expect(element).toBeTruthy();
-    expect(element.innerHTML).toBe(
-      'this is a link <a href="https://google.com">https://google.com</a>'
+  });
+
+  it('displays blue text', () => {
+    const element = getElement('\u001b[34mHello World\u001b[0m', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(0, 0, 128);">Hello World</span>'
     );
+  });
+
+  it('displays cyan background', () => {
+    const element = getElement('\u001b[46mHello World\u001b[0m', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="background-color: rgb(0, 128, 128);">Hello World</span>'
+    );
+  });
+
+  it('displays silver background', () => {
+    const element = getElement('\u001b[47mHello World', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="background-color: rgb(192, 192, 192);">Hello World</span>'
+    );
+  });
+
+  it('displays purple background', () => {
+    const element = getElement('\u001b[45mHello World', 'Hello World');
+    expect(element.outerHTML).toBe(
+      '<span style="background-color: rgb(128, 0, 128);">Hello World</span>'
+    );
+  });
+
+  it('displays red text without a trailing reset', () => {
+    const element = getElement('\u001b[36mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(0, 128, 128);">Hello</span>'
+    );
+  });
+
+  it('displays red on blue', () => {
+    const element = getElement('\u001b[31;44mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 0, 0); background-color: rgb(0, 0, 128);">Hello</span>'
+    );
+  });
+
+  it('resets colors after red on blue', () => {
+    const element = getElement('\u001b[31;44mHello\u001b[0m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 0, 0); background-color: rgb(0, 0, 128);">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('performs color change from red/blue to yellow/blue', () => {
+    const element = getElement('\u001b[31;44mHello\u001b[33m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 0, 0); background-color: rgb(0, 0, 128);">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe(
+      '<span style="color: rgb(128, 128, 0); background-color: rgb(0, 0, 128);"> world</span>'
+    );
+  });
+
+  it('performs color change from red/blue to yellow/blue', () => {
+    const element = getElement(
+      '\u001b[31;44mHello\u001b[33;42m world',
+      'Hello'
+    );
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(128, 0, 0); background-color: rgb(0, 0, 128);">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe(
+      '<span style="color: rgb(128, 128, 0); background-color: rgb(0, 128, 0);"> world</span>'
+    );
+  });
+
+  it('ignores unsupported codes', () => {
+    const element = getElement('\u001b[51mHello\u001b[0m', 'Hello');
+    expect(element.outerHTML).toBe('<span>Hello</span>');
+  });
+
+  it('displays light red', () => {
+    const element = getElement('\u001b[91mHello\u001b[0m', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(255, 0, 0);">Hello</span>'
+    );
+  });
+
+  it('displays light red', () => {
+    const element = getElement('\u001b[101mHello\u001b[0m', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="background-color: rgb(255, 0, 0);">Hello</span>'
+    );
+  });
+
+  it('sets background xterm color', () => {
+    const element = getElement('\u001b[48;5;200mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="background-color: rgb(255, 0, 215);">Hello</span>'
+    );
+  });
+
+  it('sets foreground xterm color', () => {
+    const element = getElement('\u001b[38;5;100mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="color: rgb(135, 135, 0);">Hello</span>'
+    );
+  });
+
+  it('displays bold text', () => {
+    const element = getElement('\u001b[1mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="font-weight: bold;">Hello</span>'
+    );
+  });
+
+  it('can reset bold text', () => {
+    const element = getElement('\u001b[1mHello\u001b[21m world', 'Hello');
+    const element2 = getElement('\u001b[1mHello\u001b[22m world', 'Hello');
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+    expect(element2.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('displays italic text', () => {
+    const element = getElement('\u001b[3mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="font-style: italic;">Hello</span>'
+    );
+  });
+
+  it('can reset italic text', () => {
+    const element = getElement('\u001b[3mHello\u001b[23m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="font-style: italic;">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('displays underline text', () => {
+    const element = getElement('\u001b[4mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="text-decoration: underline;">Hello</span>'
+    );
+  });
+
+  it('can resets underline text', () => {
+    const element = getElement('\u001b[4mHello\u001b[24m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="text-decoration: underline;">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('displays concealed text', () => {
+    const element = getElement('\u001b[8mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="visibility: hidden;">Hello</span>'
+    );
+  });
+
+  it('can resets concealed text', () => {
+    const element = getElement('\u001b[8mHello\u001b[28m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="visibility: hidden;">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('displays cross text', () => {
+    const element = getElement('\u001b[9mHello', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="text-decoration: line-through;">Hello</span>'
+    );
+  });
+
+  it('can resets cross text', () => {
+    const element = getElement('\u001b[9mHello\u001b[29m world', 'Hello');
+    expect(element.outerHTML).toBe(
+      '<span style="text-decoration: line-through;">Hello</span>'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe('<span> world</span>');
+  });
+
+  it('display links', () => {
+    const element = getElement(
+      'A dashboard for Tekton! https://github.com/tektoncd/dashboard',
+      'A dashboard for Tekton!'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe(
+      '<a href="https://github.com/tektoncd/dashboard">https://github.com/tektoncd/dashboard</a>'
+    );
+  });
+
+  it('display links with styles', () => {
+    const element = getElement(
+      'A dashboard for Tekton!\u001b[9m\u001b[48;5;194mhttps://github.com/tektoncd/dashboard',
+      'A dashboard for Tekton!'
+    );
+    expect(element.nextElementSibling.outerHTML).toBe(
+      '<a href="https://github.com/tektoncd/dashboard" style="background-color: rgb(215, 255, 215); text-decoration: line-through;">https://github.com/tektoncd/dashboard</a>'
+    );
+  });
+
+  it('seperates text by new lines', () => {
+    const text =
+      'Hello World\nA dashboard for Tekton! https://github.com/tektoncd/dashboard\nTekon is cool!';
+    const { container } = renderWithIntl(<Ansi2html>{text}</Ansi2html>);
+    expect(container.childNodes).toHaveLength(3);
+  });
+
+  it('seperates text by new lines and carriage returns', () => {
+    const text = '\r \n \r \n\r \n';
+    const { container } = renderWithIntl(<Ansi2html>{text}</Ansi2html>);
+    expect(container.childNodes).toHaveLength(4);
   });
 });
